5ced7006c2ea1af76aa6f85573df6dd6dfa1c161
Алёна Павлова <alyonabill3@mail.ru>
2018-09-05 09:29:41 +0300

    Refactoring

--- routes/views/test.js
+++ routes/views/test.js
@@ -21,8 +21,8 @@ module.exports = async function (req, res) {
 			locals.data.tags = results;
 
 			// Load the counts for each tag
-			async.each(locals.data.tags, (tag, next) => {
-				Note.count().where('state', 'published').where('tags').in([tag.id]).then(count => { tag.noteCount = count }).catch(err => next(err));
+			async.each(locals.data.tags, async (tag, next) => {
+				await Note.count().where('state', 'published').where('tags').in(tag.id).then(count => { tag.noteCount = count }).catch(err => next(err));
 			});
 		}).catch(err => next(err));
 		next();
@@ -30,12 +30,12 @@ module.exports = async function (req, res) {
 
 	// Load the current tag filter
 	view.on('init', async next => {
-		req.params.tag ? await Tag.findOne({ key: locals.filters.tag }).exec((err, result) => { locals.data.tag = result; next(err) }) : next();
+		req.params.tag ? await Tag.findOne({ key: locals.filters.tag }).then(result => { locals.data.tag = result }).catch(err => next(err)): next()
 	});
 
 	// Load the notes
 	view.on('init', async next => {
-		let notes = keystone.list('Note').paginate({
+		let notes = Note.paginate({
 			page: req.query.page || 1,
 			perPage: 10,
 			maxPages: 10,
@@ -43,10 +43,11 @@ module.exports = async function (req, res) {
 		}).sort('-publishedDate').populate('author tags');
 
 		if (locals.data.tag) {
-			notes.where('tags').in([locals.data.tag]);
+			notes.where('tags').in(locals.data.tag);
 		}
 
-		notes.exec((err, results) => { locals.data.notes = results; next(err) });
+		notes.then(results => { locals.data.notes = results }).catch(err => { return err });
+		next();
 	});
 
 	view.render('notes');
