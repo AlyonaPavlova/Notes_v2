3c667d255485a864990d0fdcfa90b4abeaf04a10
AlyonaPavlova <alyonabill3@mail.ru>
2018-09-07 11:26:47 +0300

    Update updateNote.js

--- routes/views/updateNote.js
+++ routes/views/updateNote.js
@@ -4,25 +4,23 @@ const Note = keystone.list('Note').model;
 const Tag = keystone.list('Tag').model;
 
 module.exports = async function (req, res) {
-
 	const view = new keystone.View(req, res);
 	const locals = res.locals;
 
 	locals.formData = req.body || {};
 	locals.section = 'updateNote';
 	
-	const note = await Note.findOne({ slug: req.params.note }).then(note => { return note }).catch(err => { return err });
-	const tags = note.tags;
+	const note = await Note.findOne({ slug: req.params.note }).populate('tags');
 	const tagsNames = [];
-	
-	for (let i = 0; i < tags.length; i++) {
-		await Tag.findById(tags[i]).then(tag => tagsNames.push(tag.name)).catch(err => { return err });
+
+	for (let i = 0; i < note.tags.length; i++) {
+		tagsNames.push(note.tags[i].name);
 	}
 
 	locals.note = note;
 	locals.tags = tagsNames.join(', ');
 
-	view.on('post', { action: 'note.update' }, async () => {
+	view.on('post', { action: 'note.update' }, async next => {
 		const tagsArr = [];
 		const oldTagsArr = [];
 		
@@ -32,50 +30,34 @@ module.exports = async function (req, res) {
 			for (let i = 0; i < tagsNamesArr.length; i++) {
 				await Tag.findOne({ name: tagsNamesArr[i] }).then(async tag => { 
 					if (tag === null) {
-						const newTag = await new Tag({ name: tagsNamesArr[i] }).save().catch(err => { return err });
+						let newTag = await new Tag({ name: tagsNamesArr[i] }).save();
 						tagsArr.push(newTag);
-					} else {
-						const oldTag = await Tag.findOne({ name: tagsNamesArr[i] }).then(tag => { return tag }).catch(err => { return err });
-						oldTagsArr.push(oldTag);
-					} }).catch(err => { return err });
+					} else { oldTagsArr.push(tag) } 
+				}).catch(err => next(err));
 			}
 		}
 		
 		async function noteUpdate(tagsArr) {
-			await Note.update(
-				{ _id: note.id },
-				{ $set: { title: locals.formData.title,
-						publishedDate: new Date(),
-						content: {
-							brief: locals.formData.brief,
-							extended: locals.formData.extended,
-						},
-						tags: tagsArr,
-						tagsCount: tagsArr.length }
-				}).then(() => res.redirect('/notes')).catch(err => { return err });
-		}
-		
-		if (tagsArr && oldTagsArr) {
-			const allTags = oldTagsArr.concat(tagsArr);
-			await noteUpdate(allTags);
+			await Note.findById(note.id).then(note => {
+				note.title = locals.formData.title;
+				note.publishedDate = new Date();
+				note.brief = locals.formData.brief;
+				note.extended = locals.formData.extended;
+				note.content = {
+					brief: note.brief,
+					extended: note.extended
+				};
+				note.tags = tagsArr;
+				note.tagsCount = tagsArr.length;
+
+				note.save();
+				res.redirect('/notes');
+			}).catch(err => next(err));
 		}
 
-		if (tagsArr && !oldTagsArr) {
-			await noteUpdate(tagsArr);
-		}
-
-		if (!tagsArr && oldTagsArr) {
-			await Note.update(
-				{ _id: note.id },
-				{ $set: { title: locals.formData.title,
-						publishedDate: new Date(),
-						content: {
-							brief: locals.formData.brief,
-							extended: locals.formData.extended,
-						} }
-				}).then(() => res.redirect('/notes')).catch(err => { return err });
-		}
-		
+		if (tagsArr && oldTagsArr) await noteUpdate(oldTagsArr.concat(tagsArr));
+		if (tagsArr && !oldTagsArr) await noteUpdate(tagsArr);
+		if (!tagsArr && oldTagsArr) await noteUpdate(oldTagsArr);
 	});
 	
 	view.render('updateNote');
