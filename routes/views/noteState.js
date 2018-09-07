c39d9d64c62f9e2e580d494273342923cdd7ba97
AlyonaPavlova <alyonabill3@mail.ru>
2018-09-07 11:34:25 +0300

    Update noteState.js

--- routes/views/noteState.js
+++ routes/views/noteState.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const keystone = require('keystone');
+const _ = require('lodash');
 
 const Note = keystone.list('Note').model;
 const Like = keystone.list('Like').model;
@@ -7,19 +10,25 @@ const statistics = require('./statistics');
 module.exports = async function (req, res) {
 	const note = await Note.findOne({ slug: req.params.note });
 	const currentState = await Like.findOne({ user: req.user._id, note: note._id });
-	
+	const lastTenNotes = await Note.find({ author: note.author }).sort('-publishedDate').limit(10);
+	const noteInTheLastTen = _.unionBy(lastTenNotes, note);
+
 	if (currentState === null) {
 		await new Like({
 			user: req.user,
 			note: note,
 			state: req.body.like 
 		}).save().then(async () => {
-			await statistics.newLikesCount(note.author);
-			await statistics.getLikesCountByTenNotes(note.author);
-			await statistics.getRating(note.author);
-			await statistics.getRatingByLastTenNotes(note.author);
+			req.body.like ? await statistics.getLikesCount(note.author, 1) : await statistics.getLikesCount(note.author, -1);
 			
-			res.redirect('/notes')
+			if (noteInTheLastTen !== null) {
+				req.body.like ? await statistics.getLikesCountByTenNotes(note.author, 1) : await statistics.getLikesCountByTenNotes(note.author, -1);
+			}
+			
+			await Promise.all([
+				statistics.getRating(note.author),
+			 	statistics.getRatingByLastTenNotes(note.author)
+			]).then(() => res.redirect('/notes')).catch(err => { return err });
 		}).catch(err => { return err });
 	}
 
@@ -27,11 +36,17 @@ module.exports = async function (req, res) {
 		record.state = req.body.like;
 		record.save();
 	}).then(async () => {
-		req.body.like ? await statistics.getLikesCount(note.author, 1) : await statistics.getLikesCount(note.author, -1);
-		await statistics.getLikesCountByTenNotes(note.author);
-		await statistics.getRating(note.author);
-		await statistics.getRatingByLastTenNotes(note.author);
+		if (currentState.state === true && !req.body.like) await statistics.getLikesCount(note.author, -1);
+		if (currentState.state === false && req.body.like) await statistics.getLikesCount(note.author, 1);
 
-		res.redirect('/notes')
+		if (noteInTheLastTen !== null) {
+			if (currentState.state === true && !req.body.like) await statistics.getLikesCountByTenNotes(note.author, -1);
+			if (currentState.state === false && req.body.like) await statistics.getLikesCountByTenNotes(note.author, 1);
+		}
+
+		await Promise.all([
+			statistics.getRating(note.author),
+			statistics.getRatingByLastTenNotes(note.author)
+		]).then(() => res.redirect('/notes')).catch(err => { return err });
 	}).catch(err => { return err });
 };
